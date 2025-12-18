4c1c607e53398e680a2a1e7f465c054d7cec57cd
Pranika Angdembe Limbu <141321142+pranikalimbu@users.noreply.github.com>
2025-12-18 13:44:10 +0545

    Fix typo in button function call some typo

--- deploy.py
+++ deploy.py
@@ -9,6 +9,6 @@ sepal_width= st.number_input(label="Sepal width", min_value=0.0, max_value=1.0)
 petal_length= st.number_input(label="Petal Length", min_value=0.0, max_value=1.0)
 petal_width= st.number_input(label="Petal width", min_value=0.0, max_value=1.0)
 sample= [[sepal_length, sepal_width, petal_length, petal_width]]
-if st.buttom("Predict"):
+if st.button("Predict"):
     prediction= model.predict(sample)[0]
     st.success(f"Predicted species is {prediction}")
